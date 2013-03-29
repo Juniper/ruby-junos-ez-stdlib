aa735b794618fad31c775b2dbfd7d88407ceace7
Jeremy Schulman <jschulman@juniper.net>
2013-03-29 15:25:04 -0400

    updates 3/29

--- tests/mylogins.rb
+++
@@ -1,6 +0,0 @@
-module MyLogins
-   HOSTS = { 
-      'ex4' => { :username => 'jeremy', :password => 'jeremy1', :target => 'ex4' },
-      'coffeebite' => { :username => 'jeremy', :password => 'jeremy1', :target => 'coffeebite.englab.juniper.net' }
-   }
-end
