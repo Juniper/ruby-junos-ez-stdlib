8fc403333105251d32d0e5e2362be66b7b7e171e
Jeremy Schulman <jschulman@juniper.net>
2013-04-21 18:07:05 -0400

    updates

---
+++ lib/junos-ez/loader.rb
@@ -0,0 +1,94 @@
+=begin
+---------------------------------------------------------------------
+Loader::Utils is a collection of methods used for loading 
+configuration files/templates and software images
+---------------------------------------------------------------------
+=end
+
+module Junos::Ez::Loader  
+  def self.Utils( ndev, varsym )            
+    newbie = Junos::Ez::Loader::Provider.new( ndev )      
+    Junos::Ez::Provider.attach_instance_variable( ndev, varsym, newbie )    
+  end          
+end
+
+### -----------------------------------------------------------------
+###                        PUBLIC METHODS
+### -----------------------------------------------------------------
+### -----------------------------------------------------------------
+
+class Junos::Ez::Loader::Provider < Junos::Ez::Provider::Parent
+  
+  ### ---------------------------------------------------------------
+  ### image! - used to load a software image onto device
+  ###
+  ### options (opts)  
+  ### :source => URL -- path to image
+  ### :destination => path -- performs copy of image here from :source
+  ### :no_validate => true -- do not validate 
+  ### :reboot => true -- reboot once the loading completes  
+  ### ---------------------------------------------------------------
+  
+  def image!( opts = {} )
+  end
+  
+  ### ---------------------------------------------------------------
+  ### config! - used to load configuration files / templates.  This
+  ###    does not perform a 'commit', just the equivalent of the
+  ###    load-configuration RPC
+  ###
+  ### options:
+  ### :filename => path - indcates the filename of content
+  ###    note: filename extension will also define format
+  ###    .conf <==> :text
+  ###    .xml  <==> :xml
+  ###    .{set,text,txt}  <==> :set
+  ### :format =>  [:text, :set, :xml], default :text (curly-brace)
+  ###    this will override any auto-format from the :filename
+  ### :content => String - string content of data (vs. :filename)
+  ### :template  - indicates file/content is an ERB
+  ###    => true - used current binding context
+  ###    => <object> - will grab the binding from this object
+  ###    => <binding> - will use this binding
+  ### ---------------------------------------------------------------
+  
+  def config!( opts = {} )
+  end
+  
+  ### ---------------------------------------------------------------
+  ### commit! - commits the configuration to the device
+  ### 
+  ### options:
+  ### :confirmed => timeout 
+  ### :comment => commit log comment
+  ### ---------------------------------------------------------------
+  
+  def commit!( opts {} )
+  end
+
+  ### ---------------------------------------------------------------
+  ### commit? - perform commit configuration check and reports
+  ### results in a Hash
+  ### ---------------------------------------------------------------
+
+  def commit?
+  end
+  
+  ### ---------------------------------------------------------------
+  ### rollback! - used to rollback the configuration
+  ### ---------------------------------------------------------------
+  
+  def rollback!( rollback_id = 0 )
+  end
+
+  ### ---------------------------------------------------------------
+  ### config_compare - displays diff (patch format) between
+  ### current candidate configuration loaded and the rollback_id
+  ### ---------------------------------------------------------------
+  
+  def config_compare( rollback_id = 0 )
+  end
+  
+end # class Provider    
+
+
