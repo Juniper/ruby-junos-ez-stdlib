6edb5b359247ca3c5a3f3b3956bf66acc9ab1c27
Jeremy Schulman <jschulman@juniper.net>
2013-04-25 12:28:11 -0400

    updates

--- examples/re_upgrade.rb
+++ examples/re_upgrade.rb
@@ -11,9 +11,11 @@ login = { :target => ARGV[0], :username => 'jeremy',  :password => 'jeremy1',  }
 ## create a NETCONF object to manage the device and open the connection ...
 
 ndev = Netconf::SSH.new( login )
-$stdout.print "Connecting to device #{login[:target]} ... "
+print "Connecting to device #{login[:target]} ... "
 ndev.open
-$stdout.puts "OK!"
+puts "OK!"
+
+## attach our private & utils that we need ...
 
 Junos::Ez::Provider( ndev )
 Junos::Ez::RE::Utils( ndev, :re )
@@ -22,7 +24,7 @@ Junos::Ez::FS::Utils( ndev, :fs )
 ## upload the software image to the target device
 ## http://net-ssh.github.io/net-scp/
 
-file_name = 'junos-vsrx-12.1I20130422_2129_jni-domestic.tgz'
+file_name = 'junos-vsrx-domestic.tgz'
 file_on_server = '/home/jschulman/junos-images/vsrx/' + file_name
 file_on_junos = '/var/tmp/' + file_name
 
@@ -46,7 +48,11 @@ end
 ###
 
 md5_on_s = Digest::MD5.file( file_on_server ).to_s
-md5_on_j = ndev.fs.checksum :md5, file_on_junos
+md5_on_j = ndev.fs.checksum( :md5, file_on_junos )
+
+if md5_on_s == md5_on j
+  puts "the MD5 checksum matches"
+end
 
 binding.pry
 
