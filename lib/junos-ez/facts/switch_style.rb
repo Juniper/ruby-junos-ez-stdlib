feb6eefb7ba9e17ccccb8e9eaad176546a81346a
Ganesh Nalawade <ganesh634@gmail.com>
2013-12-17 17:36:59 +0530

    Revert "Fix for PR 945745. Update switch style value for QFX device based on Junos software version"
    This reverts commit f4ca559ffeb09c23572c18d92600a12f2e18a4b6.

--- lib/junos-ez/facts/switch_style.rb
+++ lib/junos-ez/facts/switch_style.rb
@@ -8,12 +8,6 @@ Junos::Ez::Facts::Keeper.define( :switch_style ) do |ndev, facts|
       :NONE
     when /^(ex9)|(ex43)/i
       :VLAN_L2NG
-    when /^(qfx5)|(qfx3)/i
-      if  facts[:version][0..3].to_f >= 13.2 
-        :VLAN_L2NG
-      else
-        :VLAN
-      end
     else
       :VLAN
     end        
