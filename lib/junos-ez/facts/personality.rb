e8bea9d02eb7f2304c47bc6896e1ec75b69b231b
Rudolf Gavlas <oldoldgreen@gmail.com>
2016-12-15 18:24:33 +0100

    Assign SWITCH personality also to Virtual Chassis models, which have the "model" attribute of individual routing engines prefixed with "RE-". Observed on EX3400-48T (virtual chassis) with Junos 15.1X53-D52.

--- lib/junos-ez/facts/personality.rb
+++ lib/junos-ez/facts/personality.rb
@@ -2,22 +2,33 @@ Junos::Ez::Facts::Keeper.define( :personality ) do |ndev, facts|
   uses :chassis, :routingengines
   model = facts[:hardwaremodel]
 
-  examine = ( model != "Virtual Chassis" ) ? model : facts.select {|k,v| k.match(/^RE[0..9]+/) }.values[0][:model]
+  examine = nil
+  if model == 'Virtual Chassis'
+    re = facts.detect {|k,v| k.match(/^RE\d+/)}
+    if re
+      re_model = re[1][:model]
+      if re_model
+        examine = re_model.start_with?('RE-') ? re_model[3, re_model.length] : re_model
+      end
+    end
+  else
+    examine = model
+  end
 
   facts[:personality] = case examine
-  when /^(EX)|(QFX)|(OCX)/i
+  when /^(?:EX)|(?:QFX)|(?:OCX)/i
     :SWITCH
   when /^MX/i
     :MX
   when /^vMX/i
     facts[:virtual] = true
     :MX
-  when /SRX(\d){3}/i
+  when /SRX(?:\d){3}/i
     :SRX_BRANCH
   when /junosv-firefly/i
     facts[:virtual] = true
     :SRX_BRANCH
-  when /SRX(\d){4}/i
+  when /SRX(?:\d){4}/i
     :SRX_HIGHEND
   end
 end
